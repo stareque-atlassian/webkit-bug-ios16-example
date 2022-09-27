4f52fddcce52f348b48376804a147cfcb4069c64
Shehab Tareque <stareque@atlassian.com>
2022-09-27 15:08:43 +1000

    Add host application to tests, still shows same issue on ios16 only

---
+++ WebKitBugExample/HostApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HostApp
+//
+//  Created by Shehab Tareque on 27/9/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebKitBugExample/HostApp/HostAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HostAppApp.swift
+//  HostApp
+//
+//  Created by Shehab Tareque on 27/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct HostAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
